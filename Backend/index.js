13b8bca5d00ff422c651e63ed01ad5ad58b34743
Your Name <you@example.com>
2025-01-20 11:21:20 +0530

    files added successfully

--- Backend/index.js
+++ Backend/index.js
@@ -5,12 +5,14 @@ import userRouter from "./routes/userRouter.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 import cors from "cors";
+import path from "path"; 
 
 dotenv.config();
 
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 5000; // Fallback to port 5000 if PORT is not defined
+const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
 // Connect to MongoDB
 connectDB();
@@ -23,6 +25,11 @@ app.use(cors());
 // Routes
 app.use('/api/user', userRouter);
 app.use('/api/chat', chatRoutes);
+
+app.use(express.static(path.join(_dirname, "frontend", "dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"));
+});
 
 // Global error handling middleware
 app.use(errorHandler); 
